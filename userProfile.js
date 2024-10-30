954df26311602ff64a55eaffbd99b21eedc075b4
MayRosario12 <162092946+MayRosario12@users.noreply.github.com>
2024-10-30 22:27:08 +0800

    Add files via upload

---
+++ userProfile.js
@@ -0,0 +1,10 @@
+// Create an object named userProfile to store user information
+var userProfile = {
+    fullName: "May Rosario",        // Replace with the user's full name
+    age: 22,                     // Replace with the user's age
+    favoriteNumber: 7,          // Replace with the user's favorite number
+    favoriteColors: ["Yellow", "Black",] // Replace with the user's favorite colors
+};
+
+// Log the entire object to the console
+console.log(userProfile);
